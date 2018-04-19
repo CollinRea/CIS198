bc1cc1b3df77c70d644591fae8a93b589528c6e1
Collin Rea <collinr01@gmail.com>
2018-04-18 21:11:20 -0400

    Tweaked to use assert_eq macro

--- hw01/src/problem2.rs
+++ hw01/src/problem2.rs
@@ -3,14 +3,12 @@ pub type Matrix = Vec<Vec<f32>>;
 
 /// Computes the product of the inputs `mat1` and `mat2`.
 pub fn mat_mult(mat1: &Matrix, mat2: &Matrix) -> Matrix {
-  if mat1.len() != mat2.len() {
-    panic!();
-  }
+  // Panic if the mat1 and mat2 are not the same size
+  assert_eq!(mat1.len(), mat2.len());
   let mut return_mat: Matrix = Vec::new();
   for (i, x) in mat1.iter().enumerate() {
-    if x.len() != mat2[i].len() {
-      panic!();
-    }
+    // Panic if each innert matrix isn't equal in size
+    assert_eq!(x.len(),mat2[i].len());
     let mut inner_vec: Vec<f32> = Vec::new();
     for (j, y) in x.iter().enumerate() {
       inner_vec.push(y * mat2[i][j]);
