736e6f7b4550ead2b2c20b804709ff9a378ba4a3
Collin Rea <collinr01@gmail.com>
2018-04-29 11:08:42 -0400

    Add insert function

--- hw02/src/first.rs
+++ hw02/src/first.rs
@@ -1,3 +1,22 @@
+/* 
+  Binary Search Tree implementation
+
+  Usage:
+    let bst = BST::new()
+      -- create a new empty Binary Search Tree
+
+    bst.insert(val) 
+      -- insert some value into the Binary Search Tree
+      -- returns true if successful or false if it fails
+    
+    bst.search(val)
+      -- search if a value is in the Binary Search Tree
+      -- returns true if it finds it, or false if not
+
+*/
+
+// ----- DATA STRUCTURE -----
+
 #[derive(Debug, PartialEq)]
 pub struct BST {
   root: Link,
@@ -7,19 +26,66 @@ impl BST {
   pub fn new() -> BST {
     BST { root: Link::Empty }
   }
+  pub fn insert(&mut self, v: i32) -> bool {
+    self.root.insert(v)
+  }
+  pub fn search(&self, v: i32) -> bool {
+    self.root.search(v)
+  }
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum Link {
   Empty,
   More(Box<Node>),
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 struct Node {
   val: i32,
   left: Link,
   right: Link,
+}
+
+
+// ----- METHODS -----
+
+impl Link {
+  fn insert(&mut self, v: i32) -> bool {
+    let prev_link = self.clone();
+    match self {
+      Link::Empty => {
+        let new_node = Node {
+          val: v,
+          left: Link::Empty,
+          right: Link::Empty,
+        };
+        *self = Link::More(Box::new(new_node))
+      },
+      Link::More(box_node) => {
+        if box_node.val > v {
+          box_node.left.insert(v);
+        }
+        if box_node.val < v {
+          box_node.right.insert(v);
+        }
+      },
+    };
+    prev_link != *self
+  }
+}
+
+// link.search(i32) -> bool (~15 lines)
+//     If searching an empty link, return false; the element can't be found.
+//     If searching a non-empty link:
+//         return true if the element is in this node; otherwise,
+//         recurse to the left if the target value is less than the node's value
+//         recurse to the right if the target value is greater than the node's value
+
+impl Link {
+  fn search(&self, v: i32) -> bool {
+    false
+  }
 }
 
 
@@ -32,6 +98,27 @@ mod test {
   #[test]
   fn run_new() {
     let test_bst = BST { root: Link::Empty };
-    assert_eq!(test_bst, BST::new());
+    let new_bst = BST::new();
+    assert_eq!(test_bst, new_bst);
+    
+  }
+
+  #[test]
+  fn test_insert() {
+    let empty_bst = BST::new();
+    let mut test_bst = BST::new();
+    assert!(test_bst.insert(1));
+    assert_ne!(empty_bst, test_bst);
+    // Make sure if you try to enter same value, it returns false
+    // And BST is unchanged
+    test_bst.insert(2);
+    assert!(!test_bst.insert(2));
+  }
+
+  #[test]
+  fn test_search() {
+    let mut bst = BST::new();
+    bst.insert(1);
+    assert!(bst.search(1));
   }
 }
